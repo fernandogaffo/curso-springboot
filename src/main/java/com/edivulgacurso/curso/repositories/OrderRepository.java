d7d6ea87753b010180406ed97aad9aefd6248f75
Fernando Gaffo <fernandogaffo@gmail.com>
2021-03-06 19:37:01 -0300

    Pedido, Instant, ISO 8601

---
+++ src/main/java/com/edivulgacurso/curso/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.edivulgacurso.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.edivulgacurso.curso.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}

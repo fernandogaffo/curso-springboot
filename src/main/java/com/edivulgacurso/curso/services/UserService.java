6d61e6a45da5c8ab3a66b6e1e3d847e16545251e
Fernando Gaffo <fernandogaffo@gmail.com>
2021-03-13 14:08:38 -0300

    Update Usuário

--- src/main/java/com/edivulgacurso/curso/services/UserService.java
+++ src/main/java/com/edivulgacurso/curso/services/UserService.java
@@ -23,12 +23,25 @@ public class UserService {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.get();
 	}
-	
+
 	public User insert(User obj) {
 		return userRepository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		userRepository.deleteById(id);
 	}
+
+	public User update(Long id, User obj) {
+		User entity = userRepository.getOne(id);
+		updateData(entity, obj);
+		return userRepository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setNome(obj.getNome());
+		entity.setEmail(obj.getEmail());
+		entity.setFone(entity.getFone());
+	}
+
 }

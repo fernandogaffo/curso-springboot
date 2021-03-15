ed1462835f35fb1463f7be8cec914a1b097e1b96
Fernando Gaffo <fernandogaffo@gmail.com>
2021-03-15 15:12:51 -0300

    Tratamento de Exceção Update

--- src/main/java/com/edivulgacurso/curso/services/UserService.java
+++ src/main/java/com/edivulgacurso/curso/services/UserService.java
@@ -3,6 +3,8 @@ package com.edivulgacurso.curso.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -34,18 +36,22 @@ public class UserService {
 
 	public void delete(Long id) {
 		try {
-			userRepository.deleteById(id);			
-		}catch(EmptyResultDataAccessException e) {
+			userRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
-		}catch(DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
 
 	public User update(Long id, User obj) {
-		User entity = userRepository.getOne(id);
-		updateData(entity, obj);
-		return userRepository.save(entity);
+		try {
+			User entity = userRepository.getOne(id);
+			updateData(entity, obj);
+			return userRepository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
